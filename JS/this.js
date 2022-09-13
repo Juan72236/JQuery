aa5cc0185636df01b6937b1dde02a6a937b366da
JuanDeveloper <juanpablov018@gmail.com>
2022-09-13 15:52:24 -0500

    jquery

---
+++ JS/this.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.caja').on('click', function(){
+        $(this).toggleClass('color'); //me permite seleccionar el elemento, en este caso la caja, en específico a afectar.
+    });
+});
